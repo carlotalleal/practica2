c2a72e5d9e0eef3dab38bfd49653be9e5fea9a9c
carlota <carlota.llealalonso@gmail.com>
2024-11-04 10:18:16 +0100

    ejercicio 13 y coments

--- codi.c
+++ codi.c
@@ -27,11 +27,13 @@ for( i = 0; i < N; i++ ){
 }
 }
 
-//ex1:
+//exercici 1:
 void PrintVect( float vect[N], int from, int numel ){
 int i;
-for( i = from; i < numel; i++ ){
+//se declaran i definen las variables
+for( i = from; i < (numel+ from); i++ ){
     printf("%.2f ", vect[i]);
+//printear el vector con 2 decimales de precisión
 }
 }
 
@@ -43,23 +45,28 @@ for (i=from;i<numel;i++){
 }
 }
 
-//ex3:
+//exercici 3:
 void MultEscalar( float vect[N], float vectres[N], float alfa ){
     int i;
+//se declaran i definen las variables
     for( i = 0; i < N; i++ ){
         vectres[i]= vect[i]*alfa;
+//mulpiplicar cada elemonto del vector por alfa(un número)
     } 
 }
 
-//ex4:
+//exercici 4:
 float Scalar( float vect1[N], float vect2[N] ){
     int i;
     float pe;
     pe= 0;
+//se declaran i definen las variables
     for( i = 0; i < N; i++ ){
         pe= pe + (vect1[i] * vect2[i]);
+//se suman los elementos de v1 y v2 con el mismo incice
     }
     return(pe);
+//se devuelve toda la suma acumulada
 }
 
 //exercici 5
@@ -73,60 +80,83 @@ float Magnitude( float vect[N] ){
     return (res);
 }
 
-//ex6:
+//exercici 6:
 int Ortogonal( float vect1[N], float vect2[N] ){
     int ort;
     float pe;
+//se declaran i definen las variables
     pe= Scalar( vect1,vect2);
+//se utiliza la función anterior para hacer el rpoducto escalar
     if (pe){
         ort=1;
     }else {
         ort=0;
     }
+//se mira si es ortogonal o no
     return(ort);
 }
 
-//ex8
+//exercici 7
+void Projection(float vect1[N],float vect2[N],float vectres[N]){
+int pe;
+pe= Scalar (vect1,vect2);
+int mod;  
+mod= Magnitude (vect2);
+int temp;
+temp: pe/mod;
+MultEscalar(vect2,vectres,temp);
+}
+
+//exercici 8
 float Infininorm( float M[N][N] ){
      int i, ii;
      float comp, acum, m;
      comp = 0;
+//se declaran i definen las variables
      for( i = 0; i < N; i++ ){
         acum = 0;
+//declaramos y reiniciamos el acumulador a 0 cada que cambia la fila
         for (ii = 0; ii < N; ii++){
             m= fabs(M[i][ii]);
             acum = m + acum;
+//se acumula la suma de la linea completa
         }
         if (acum > comp){
             comp = acum;
+//se mira si es mayor que el comparador(la fila mayor hasta ese momento)
         }
      }
      return(comp);
 }
 
-//ex9
+//exercici 9
 float Onenorm( float M[N][N] ){
     int i, ii;
     float comp, acum, m;
     comp = 0;
+//se declaran i definen las variables
     for( i = 0; i < N; i++ ){
        acum = 0;
+//declaramos y reiniciamos el acumulador a 0 cada que cambia la columna
        for (ii = 0; ii < N; ii++){
            m= fabs(M[ii][i]);
            acum = m + acum;
+//se acumula la suma de la columna completa
        }
        if (acum > comp){
            comp = acum;
+//se mira si es mayor que el comparador(la fila mayor hasta ese momento)
        }
     }
     return(comp);
 }
 
 
-//ex10
+//exercici 10
 float NormFrobenius( float M[N][N] ){
     int i, ii;
     float acum, m;
+//se declaran i definen las variables
     for( i = 0; i < N; i++ ){
         for (ii = 0; ii < N; ii++){
             m= pow(M[i][ii], 2);
@@ -136,18 +166,56 @@ float NormFrobenius( float M[N][N] ){
     return(acum);
 }
 
-
-//ex11
+//exercici 11
 int DiagonalDom( float M[N][N] ){
     int i, ii, DD;
     float acum, m;
     ii=0;
-    acum =0;
+    acum=0;
+    DD= 1;
+//se declaran i definen las variables
     for( i = 0; i < N; i++ ){
-        if (ii!=i){
-            m = fabs(M[ii][i]);
-            acum = acum + m;
+        acum = 0;
+//declaramos y reiniciamos el acumulador a 0 cada que cambia la fila
+        for (ii = 0; ii < N; ii++ ){
+            if (ii!=i){
+//se mira si el numero de filas es igual al numero de columnas 
+                m = fabs(M[ii][i]);
+                acum = acum + m;
+//se acumula la linea exclyendo la orden anterior
+            }
         }
-        ii= ii+1;
+        if (acum < M[i][i]);
+            DD= 0;
+//se mira si es de diagonal dominante o no
     }
+    return(DD);
+}
+
+//exercici 13
+int Jacobi( float M[N][N] , float vect[N], float vectres[N], unsigned iter ){
+    int DD,Jb;
+    float xN[N], s;
+    iter = 0;
+    DD= DiagonalDom(M);
+    if (DD==1){
+        vectres[0]= 0;
+        for (int v = 0; v < iter; v++){
+            for (int i = 0; i < N; i++) {
+                s = 0;
+                for (int ii = 0; ii < N; ii++) {
+                    if (i != ii) {
+                        s += M[i][ii] * vectres[ii-1];
+                    }
+                }
+                xN[i] = (vect[i] - s) / M[i][i];
+            }
+            for (int i = 0; i < N; i++){
+                vectres[i]= xN[i];
+            }
+        }
+    }
+    else{
+        Jb= 0;
+    } 
 }
